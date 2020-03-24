2465f3e459770cdcadfd741e3e357e31866149ad
Gotowy <pszygoda@gmail.com>
2020-03-24 10:23:25 +0100

    Updates

---
+++ precache-manifest.70257ef027e8c7bcc70efc68305cb1e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b4dcf3b9fd2bebe37e47ae0fa28e10",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "ff157d6530ba8578c4bb",
+    "url": "/movie-app/static/css/main.98a302f2.chunk.css"
+  },
+  {
+    "revision": "ca4d26ff4f88b1e9f6e5",
+    "url": "/movie-app/static/js/2.404984fe.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-app/static/js/2.404984fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff157d6530ba8578c4bb",
+    "url": "/movie-app/static/js/main.bf294429.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
